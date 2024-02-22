55fd1381e607dc09c505cecfb221ff7ab2ca0033
arang nam <arang.nam@gmail.com>
2024-02-22 20:34:04 +0900

    모달 갤러리 초안

--- en/works/2023-02.php
+++ en/works/2023-02.php
@@ -32,62 +32,133 @@
 
             <ul class="list_gallery">
               <li>
-                <img src="../../include/images/2023_02_04_01.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-1">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_01.jpg">
+                  <img src="../../include/images/2023_02_04_01.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-1">
+                  <p></p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_02.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-2">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_02.jpg">
+                  <img src="../../include/images/2023_02_04_02.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-2">
+                  <p>&lt;Engine Fan Blade - General Cindy&gt; 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_03.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-3">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_03.jpg">
+                  <img src="../../include/images/2023_02_04_03.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-3">
+                  <p>&lt;Engine Fan Blade - Grandpa Nyang Nyang&gt; 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_04.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-4">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_04.jpg">
+                  <img src="../../include/images/2023_02_04_04.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-4">
+                  <p>&lt;Engine Fan Blade - The Sparow&gt; 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_05.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-5">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_05.jpg">
+                  <img src="../../include/images/2023_02_04_05.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-5">
+                  <p>&lt;Engine Fan Blade - Emperor Ching Ching&gt; 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_06.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-6">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_06.jpg">
+                  <img src="../../include/images/2023_02_04_06.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-6">
+                  <p>&lt;Engine Fan Blade - Zhang Lung Chih> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_07.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-7">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_07.jpg">
+                  <img src="../../include/images/2023_02_04_07.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-7">
+                  <p>&lt;Engine Fan Blade - Sisters Ling Ling and Ming Ming> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_08.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-8">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_08.jpg">
+                  <img src="../../include/images/2023_02_04_08.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-8">
+                  <p>&lt;Engine Fan Blade - Ding Ding Mo> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_09.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-9">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_09.jpg">
+                  <img src="../../include/images/2023_02_04_09.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-9">
+                  <p>&lt;Engine Fan Blade - Genius Philosopher Singh> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_10.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-10">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_10.jpg">
+                  <img src="../../include/images/2023_02_04_10.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-10">
+                  <p>&lt;Engine Fan Blade - Michael the Caucassian> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_11.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-11">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_11.jpg">
+                  <img src="../../include/images/2023_02_04_11.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-11">
+                  <p>&lt;Engine Fan Blade - The Ping> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_12.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-12">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_12.jpg">
+                  <img src="../../include/images/2023_02_04_12.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-12">
+                  <p>&lt;Engine Fan Blade - Wang Ying> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_13.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-13">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_13.jpg">
+                  <img src="../../include/images/2023_02_04_13.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-13">
+                  <p>&lt;Engine Fan Blade - Princes Ching Ching> 87x40x27cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_14.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-14">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_14.jpg">
+                  <img src="../../include/images/2023_02_04_14.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-14">
+                  <p>&lt;A Piece from Engine - The People Who Painted Ching Chen&gt; 77x100x12cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_15.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-15">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_15.jpg">
+                  <img src="../../include/images/2023_02_04_15.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-15">
+                  <p>&lt;Broken Engine Fan Blade&gt; Acrylic and mixed media on FRP, 60 x 40 x 15cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_16.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-16">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_16.jpg">
+                  <img src="../../include/images/2023_02_04_16.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-16">
+                  <p>&lt;Broken Engine Fan Blade&gt; Acrylic and mixed media on FRP, 60 x 40 x 15cm</p>
+                </a>
               </li>
               <li>
-                <img src="../../include/images/2023_02_04_17.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-17">
+                <a href="#" data-fancybox="single" data-src="../../include/images/2023_02_04_17.jpg">
+                  <img src="../../include/images/2023_02_04_17.jpg" alt="Big King Airlines New Engine Fundraising Drive image4-17">
+                  <p>&lt;Broken Engine Spinner&gt; Acrylic and mixed media on FRP, 50 x 50 x 47cm</p>
+                </a>
               </li>
             </ul>
+            <!-- modal gallery -->
+            <div class="gallery" id="modalOverlay">
+              <div class="modal-content" id="modalContent">
+                <!-- Your content here -->
+                <img src="" id="zoomableImage">
+              </div>
+              <div id="hud"></div>
+                <!-- <a class="close" href="#">
+                    <i>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    </i>
+                </a> -->
+            </div>
             <div class="paging"><span class="current">1</span>/<span class="sum">17</span></div>
 
+            <p class="txt_work bottom">
+              <a href="https://www.bigkingair.com/propeller" target="_blank" class="link_go">Click</a>
+               to learn more about each segment's story
+            </p>
+
             <img src="../../include/images/2023_02_05.jpg" alt="Big King Airlines New Engine Fundraising Drive image5">
-            <!-- <p class="txt_work bottom vertical">
-            </p> -->
+            <p class="txt_work bottom">
+              Exhibition display (CYLINDER 2, 2023) (Photo : CYLINDER)
+            </p>
 
             <div class="thumb_area">
               <!-- 정사각 -->
@@ -127,13 +198,26 @@
                 </li>
               </ul> -->
             </div>
+            <p class="txt_work bottom">
+              (These promotional materials were created in collaboration with marketing agency  
+              <a href="https://streaklinks.com/B2dXEQUSUkM2rY1Yow1FQGTs/https%3A%2F%2Fwww.instagram.com%2Fstudio.yonke%2F" target="_blank" class="link_go">Studio. Yonke.</a> 
+              Click the thumbnails to explore the materials.)
+            </p>
 
             <img src="../../include/images/2023_02_06.jpg" alt="Big King Airlines New Engine Fundraising Drive image6">
+            <p class="txt_work bottom">
+              《Singapore Art Week 2024》  exhibition display (Singapore Art Museum, 2024)
+            </p>
+
             <img src="../../include/images/2023_02_07.jpg" alt="Big King Airlines New Engine Fundraising Drive image7">
-            <!-- <p class="txt_work bottom vertical">
-            </p> -->
+            <p class="txt_work bottom">
+              《Transmediale 2024 : this is perfect, perfect, perfect》 exhibition display, Kunstraum Kreuzberg/Bethanien, Berlin
+            </p>
 
           </div>
+
+          <!-- <script type="text/javascript" src="../../include/js/pinch.js"></script> -->
+
       </div>
 
       <!-- //content -->
